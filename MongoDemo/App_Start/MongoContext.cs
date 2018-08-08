3b5c6f796e932ed16901eede8107dc9cb1219ae3
zerman <zerman.demma@gmail.com>
2018-08-08 16:23:18 -0400

    Add project files.

---
+++ MongoDemo/App_Start/MongoContext.cs
@@ -0,0 +1,38 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace MongoDemo.App_Start
+{
+    public class MongoContext
+    {
+        MongoClient _client;
+        MongoServer _server;
+        public MongoDatabase _database;
+        public MongoContext() //constructor   
+        {
+            // Reading credentials from Web.config file   
+            var MongoDatabaseName = ConfigurationManager.AppSettings["MongoDatabaseName"]; //CarDatabase  
+           // var MongoUsername = ConfigurationManager.AppSettings["MongoUsername"];   
+           // var MongoPassword = ConfigurationManager.AppSettings["MongoPassword"];   
+            var MongoPort = ConfigurationManager.AppSettings["MongoPort"];  //27017  
+            var MongoHost = ConfigurationManager.AppSettings["MongoHost"];  //localhost  
+          
+            var settings = new MongoClientSettings
+            {
+                  // Credentials = new[] { credential },
+                  Server = new MongoServerAddress(MongoHost, Convert.ToInt32(MongoPort))//For Local
+                  // Server = new MongoServerAddress(mongoUrl.ToString())
+            };
+
+            _client = new MongoClient(settings); //For local
+
+            _server = _client.GetServer();//For Local
+             _database = _server.GetDatabase(MongoDatabaseName); // For Local
+
+        }
+    }
+}
---
+++ MongoDemo/Models/CarModel.cs
@@ -0,0 +1,35 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MongoDemo.Models
+{
+    public class CarModel
+    {   [BsonId]
+        public ObjectId Id { get; set; }
+        [BsonElement("Carname")]
+        public string Carname { get; set; }
+        [BsonElement("Color")]
+        public string Color { get; set; }
+        [BsonElement("Price")]
+        public int Price { get; set; }
+        [BsonElement("Engineno")]
+        public string Engineno { get; set; }
+        [BsonElement("Registrationno")]
+        public string Registrationno { get; set; }
+        [BsonElement("RegistrationDate")]
+        public string RegistrationDate { get; set; }
+        [BsonElement("Model")]
+        public string Model { get; set; }
+        [BsonElement("Chassisno")]
+        public string Chassisno { get; set; }
+
+        [BsonElement("Title")]
+        public string Title { get; set; }
+
+
+    }
+}
